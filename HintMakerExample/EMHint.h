4fc1dcd357a85f96bff97e1e772e2908883336c5
Cory Alder <coryalder@gmail.com>
2012-11-25 22:47:24 -0800

    Fixed a crash when you quickly release EMHint after showing.
    On line 34 EMHint calls
    
       -()animateWithDuration:delay:options:animations:completion:
    
    If by the time this is called, EMHint's `hintDelegate` has been released, it will crash.
    This not even actually that hard to trigger. I was hitting it with arc by opening and closing the hint view rapidly.
    
    I fixed this by making EMHint retain its `hintDelegate`

--- HintMakerExample/EMHint.h
+++ HintMakerExample/EMHint.h
@@ -80,7 +80,7 @@ typedef enum
 {
     EMHintsView *_modalView;//our transparent hint window with lablel and spotlight
 }
-@property (nonatomic,assign) id<EMHintDelegate> hintDelegate;
+@property (nonatomic,retain) id<EMHintDelegate> hintDelegate;
 
 -(UIView*)modalView; // accessor to the modal view (use spareingly)
 -(void)clear;//instant removal of modal view
--- HintMakerExample/EMHint.m
+++ HintMakerExample/EMHint.m
@@ -136,6 +136,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #pragma mark ---------------------------------->> 
 #pragma mark -------------->>cleanup
 - (void)dealloc {
+    self.hintDelegate = nil;
     [_modalView release];
     [super dealloc];
 }
